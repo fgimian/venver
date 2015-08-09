ca62d0810e3589167b0e97e0b70794acbb037f42
Fotis Gimian <fgimiansoftware@gmail.com>
2015-08-09 23:08:31 +1000

    Touched up layout of tests

---
+++ test/test_help.py
@@ -0,0 +1,14 @@
+from .helpers import venv_tempdir, run
+
+
+def test_help_is_shown(venv_tempdir):
+    rv = run(args='', venv_dir=venv_tempdir, shell='bash')
+    assert rv.returncode == 1
+    assert 'Usage: venv <command> [<args>]' in rv.stdout
+
+
+def test_invalid_argument(venv_tempdir):
+    rv = run(args='booboo', venv_dir=venv_tempdir, shell='bash')
+    assert rv.returncode == 1
+    assert 'Usage: venv <command> [<args>]' in rv.stdout
+    assert 'venv: unsupported command booboo' in rv.stdout
